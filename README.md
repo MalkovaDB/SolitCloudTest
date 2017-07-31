f6c557e7d2101066e34189475b188f5d845cb26c
gravicapa <dasha.m91@gmail.com>
2017-07-31 11:22:24 +0300

    add completed test

---
+++ README.md
@@ -0,0 +1,13 @@
+# README #
+
+Для запуска теста необходимо иметь IntelliJ + jdk, Selenium web driver, и gecko driver (при использовании selenium web driver ver. > 3.0).
+
+1. Для того, чтобы добавить .jar файлы Selenium'a в IntelliJ в качестве внешних библиотек:
+
+	* Кликнуть File -> Project Structure -> в настройках проекта перейти на вкладку Modules -> Dependencies -> кликнуть на '+' Sign -> выберите JAR файлы.
+
+
+2. Для того, чтобы установить Gecko driver:
+
+	* Скачать нужную версию (https://github.com/mozilla/geckodriver/releases), разархивировать в любое место.  
+	* Добавить путь к Gecko driver в переменную Path.
---
+++ test/src/test.java
@@ -0,0 +1,79 @@
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class test {
+    private WebDriver driver;
+    private String baseUrl;
+    private StringBuffer errors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://www.google.com/ncr";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void test() throws Exception {
+        String SEARCHRESULTXPATH = "//*[@class='srg']//h3/a";
+        String IMAGERESULTXPATH = "//div[@id='rg_s']//a";
+        String SELENIUMSITE = "seleniumhq.org";
+
+        driver.get(baseUrl);
+        driver.findElement(By.id("lst-ib")).clear();
+        driver.findElement(By.id("lst-ib")).sendKeys("selenium");
+        driver.findElement(By.id("lst-ib")).submit();
+
+        final Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
+        wait.until(ExpectedConditions.visibilityOfAllElements(driver.findElements(By.xpath(SEARCHRESULTXPATH))));
+
+        String foundResult = driver.findElements(By.xpath(SEARCHRESULTXPATH)).get(0).getAttribute("href");
+
+        try{
+            assertTrue(foundResult.contains(SELENIUMSITE));
+        }
+        catch (Error e) {
+            errors.append(e.toString() + ". The result link does not apply to \"seleniumhq.org\" website.");
+        }
+
+        driver.findElement(By.linkText("Images")).click();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElements(By.xpath(IMAGERESULTXPATH)).get(0)));
+
+        String foundImage = driver.findElements(By.xpath(IMAGERESULTXPATH)).get(0).getAttribute("href");
+
+        try{
+            assertTrue(foundImage.contains(SELENIUMSITE));
+        }
+        catch (Error e) {
+            errors.append(e.toString() + ". Image link does not apply to \"seleniumhq.org\" website.");
+        }
+
+        driver.findElement(By.linkText("All")).click();
+        wait.until(ExpectedConditions.visibilityOfAllElements(driver.findElements(By.xpath("//*[@class='srg']//h3/a"))));
+
+        String repeatResult = driver.findElements(By.xpath(SEARCHRESULTXPATH)).get(0).getAttribute("href");
+
+        try{
+            assertEquals(foundResult, repeatResult);
+        }
+        catch (Error e) {
+            errors.append(e.toString());
+        }
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        if (!"".equals(errors.toString())) {
+            fail(errors.toString());
+        }
+    }
+}
